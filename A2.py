e6ae8a14afd25d598d862b38b5f4a846434ba1f7
Moaaz Hasan <mo2222ath@gmail.com>
2020-11-19 18:06:22 +0200

    Add Representation, Fitness and showChromosome functions.

--- A2.py
+++ A2.py
@@ -1,8 +1,43 @@
 import random
 
-def Representation(numberOfChannels, ROIDictionary, boundariesList, totalBudget):
-    pass
 
+def Representation(ROIDictionary, boundariesList, totalBudget):
+    copyBoundariesList = boundariesList[:]
+    copyROIDictionary = ROIDictionary
+    allowedBudget = 0
+    lowerSum = 0
+    genes = []
+    channelsNames = list(ROIDictionary.keys())
+    channelsRIO = list(ROIDictionary.values())
+    c = 0
+    for item in copyBoundariesList:
+        gene = Gene()
+        if item[0] == 'x':
+            item[0] = 0
+        if item[1] == 'x':
+            item[1] = 0
+        item[1] = (item[1] / 100) * totalBudget
+        lowerSum += item[0]
+        gene.lowerBound = item[0]
+        gene.upperBound = item[1]
+        gene.ROI = channelsRIO[c]
+        gene.channelName = channelsNames[c]
+        c += 1
+        genes.append(gene)
+
+    allowedBudget = totalBudget - lowerSum
+
+    for gene in genes:
+        if gene.upperBound == 0:
+            gene.upperBound = gene.lowerBound + allowedBudget
+        elif allowedBudget < gene.upperBound:
+            gene.upperBound = allowedBudget
+        gene.allocatedBudgetValue = random.uniform(int(gene.lowerBound), int(gene.upperBound))
+        allowedBudget = allowedBudget - (gene.allocatedBudgetValue - gene.lowerBound)
+
+    # for item in genes:
+    #     print(item.channelName ,item.lowerBound , item.upperBound , item.allocatedBudgetValue )
+    return genes
 
 
 class GeneticA:
@@ -18,7 +53,7 @@ class GeneticA:
     def CreateNewGeneration(self, generation):
         pass
 
-    def replaceGeneration(self,oldGeneration,newGeneration):
+    def replaceGeneration(self, oldGeneration, newGeneration):
         pass
 
     def Selection(self, chromosome1, n1, chromosome2, n2):
@@ -40,11 +75,12 @@ class GeneticA:
     def nonUniformMutation(self, offspring, pm):
         pass
 
-    def checkConstraints(self, chromo, numOfW, numOfT) # genes are weapons instances divided by targets
+    def checkConstraints(self, chromo, numOfW, numOfT):
         pass
 
     def fixChromosome(self, chromosome):
         pass
+
 
 class Gene:
     ROI = 0
@@ -53,79 +89,86 @@ class Gene:
     upperBound = ""
     lowerBound = ""
 
+
 class Chromosome:
     Genes = []
-    numberOfChannels = 0
     totalBudget = 0
     fitness = 0
 
-    def __init__(self, Genes, numberOfChannels,totalBudget):
-        self.numberOfChannels = numberOfChannels
-        self.totalBudget = totalBudget
-        self.Genes = Genes
-        self.fitness = self.Fitness()
+    def __init__(self, genes, numberOfChannel, totalBudgets):
+        self.numberOfChannels = numberOfChannel
+        self.totalBudget = totalBudgets
+        self.Genes = genes
+        self.Fitness()
 
-    def SetGenes(self, Genes):
-        self.Genes = Genes
-        self.fitness = self.Fitness()
+    def SetGenes(self, genes):
+        self.Genes = genes
+        self.Fitness()
 
     def Fitness(self):
-        pass
+        for g in self.Genes:
+            self.fitness += (g.allocatedBudgetValue * g.ROI) / totalBudget
+            # print(self.fitness, g.allocatedBudgetValue, g.ROI, totalBudget)
+
+    def showChromosome(self):
+        d = []
+        for gene in self.Genes:
+            d.append(gene.allocatedBudgetValue)
+        print(d , " Fitness : " , self.fitness)
+
 
 if __name__ == '__main__':
     print("Enter the marketing budget (in thousands): ")
-    totalBudget = float(input())
+    totalBudget = 100  # float(input())
 
     print("Enter the number of marketing channels: ")
-    numberOfChannels = int(input())
+    numberOfChannels = 4  # int(input())
 
     print("Enter the name and ROI (in %) of each channel separated by space: ")
     key = ""
-    ROIDictionary = dict()
-    for i in range(numberOfChannels):
-        key = input()
-        key_value = key.split(" ")
-        if len(key_value) > 1:
-            ROIDictionary[key_value[0:len(key_value)-2]] = int(key_value[-1])
-
+    ROIDictionary = {"TV advertising": 8, "Google": 12, "Twitter": 7, "Facebook": 11}  # dict()
+    # for i in range(numberOfChannels):
+    #     key = input()
+    #     key_value = key.split(" ")
+    #     if len(key_value) > 1:
+    #         tempKey = key_value[0:len(key_value) - 1]
+    #         s = ''
+    #         for item in tempKey:
+    #             s += ' ' + item
+    #         ROIDictionary[s] = int(key_value[-1])
 
     print("Enter the lower (k) and upper bounds (%) of investment in each channel: (enter x if there is no bound)")
-    boundariesList = list()
-    for i in range(numberOfChannels):
-        line = input()
-        line = line.split(" ")
-        boundariesList.append(line)
+    boundariesList = [[2.7, 58], [20.5, 'x'], ['x', 18], [10, 'x']]
+    # for i in range(numberOfChannels):
+    #     line = input()
+    #     line = line.split(" ")
+    #     if line[0] != 'x':
+    #         line[0] = float(line[0])
+    #     if line[1] != 'x':
+    #         line[1] = float(line[1])
+    #     boundariesList.append(line)
 
     print("Output of GA: \n")
     chromosmeList1 = []
     for i in range(5):
-        Genes = Representation(numberOfChannels,ROIDictionary,boundariesList,totalBudget)
-        ch1 = Chromosome(Genes,numberOfChannels,totalBudget)
+        Genes = Representation(ROIDictionary, boundariesList, totalBudget)
+        ch1 = Chromosome(Genes, numberOfChannels, totalBudget)
         chromosmeList1.append(ch1)
 
-    print("First Gener")
-    for i in chromosmeList1:
-        print(i.Genes , i.fitness)
-
-    n = 0
-    while n < 5000:
-        ga = GeneticA(0.5 , 0.3)
-        chromosmeList2 = ga.CreateNewGeneration(chromosmeList1)
-        n += 1
-        print("iteration ",n)
-        print("Second Gener")
-        for i in chromosmeList2:
-            print(i.Genes , i.fitness)
+    print("First Generation : ")
+    for chromosome in chromosmeList1:
+        chromosome.showChromosome()
 
 
-
-
-
+    # n = 0
+    # while n < 5000:
+    #     ga = GeneticA(0.5 , 0.3)
+    #     chromosmeList2 = ga.CreateNewGeneration(chromosmeList1)
+    #     n += 1
+    #     print("iteration ",n)
+    #     print("Second Gener")
+    #     for i in chromosmeList2:
+    #         print(i.Genes , i.fitness)
 
 # Constraints
-    # 1: sum of genes <= total budget
-
-
-
-
-
+# 1: sum of genes <= total budget
