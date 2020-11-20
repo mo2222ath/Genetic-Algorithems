f3b06987f1c31aa2b91b58de0c18b61f7a3d2cb8
Ahmed Osama <ahmedosama49@gmail.com>
2020-11-20 07:44:20 +0200

    Added mutation and checkConstraints functions.

--- A2.py
+++ A2.py
@@ -1,6 +1,5 @@
 import random
 
-
 def Representation(ROIDictionary, boundariesList, totalBudget):
     copyBoundariesList = boundariesList[:]
     copyROIDictionary = ROIDictionary
@@ -40,6 +39,7 @@ def Representation(ROIDictionary, boundariesList, totalBudget):
     return genes
 
 
+
 class GeneticA:
     finalResult = 0
     generation = []
@@ -53,7 +53,7 @@ class GeneticA:
     def CreateNewGeneration(self, generation):
         pass
 
-    def replaceGeneration(self, oldGeneration, newGeneration):
+    def replaceGeneration(self,oldGeneration,newGeneration):
         pass
 
     def Selection(self, generation, sizeOfMatingPool):
@@ -98,17 +98,76 @@ class GeneticA:
             return chromosome1, chromosome2
 
     def uniformMutation(self, offspring, pm):
-        pass
+        arr = offspring.Genes[:]
+        newOffspring = list()
+        for gene in arr:
+            rm = random.random()
+            if rm <= pm:
+                deltaLower = gene.allocatedBudgetValue - float(gene.lowerBound)
+                deltaUpper = (float(gene.upperBound)*offspring.totalBudget)/100 - gene.allocatedBudgetValue
 
-    def nonUniformMutation(self, offspring, pm):
-        pass
+                r1 = random.random()
+                delta = 0
+                if r1 <= 0.5:
+                    delta = deltaLower
+                elif r1 > 0.5:
+                    delta = deltaUpper
 
-    def checkConstraints(self, chromo, numOfW, numOfT):
-        pass
+                r2 = random.uniform(0,delta)
+                if delta == deltaLower:
+                    newValue = gene.allocatedBudgetValue - r2
+                elif delta == deltaUpper:
+                    newValue = gene.allocatedBudgetValue + r2
+
+                gene.allocatedBudgetValue = newValue
+                newOffspring.append(gene)
+            else:
+                newOffspring.append(gene)
+        offspring.SetGenes(newOffspring)
+        return offspring
+
+    def nonUniformMutation(self, offspring, t, T, b, pm):          # t: current gen number, T: max number of gens, b: non-uniformity factor
+        arr = offspring.Genes[:]
+        newOffspring = list()
+        for gene in arr:
+            rm = random.random()
+            if rm <= pm:
+                deltaLower = gene.allocatedBudgetValue - float(gene.lowerBound)
+                deltaUpper = (float(gene.upperBound)*offspring.totalBudget)/100 - gene.allocatedBudgetValue
+
+                r1 = random.random()
+                y = 0
+                if r1 <= 0.5:
+                    y = deltaLower
+                elif r1 > 0.5:
+                    y = deltaUpper
+
+                r = random.random()                     # to be used in Δ(t,y) eq.
+                delta_t_y = y * (1-pow(r,pow((1-t/T),b)))
+                if y == deltaLower:
+                    newValue = gene.allocatedBudgetValue - delta_t_y
+                elif y == deltaUpper:
+                    newValue = gene.allocatedBudgetValue + delta_t_y
+
+                gene.allocatedBudgetValue = newValue
+                newOffspring.append(gene)
+            else:
+                newOffspring.append(gene)
+        offspring.SetGenes(newOffspring)
+        return offspring
+
+    def checkConstraints(self, chromo, totalBudget):
+        genes = chromo.Genes[:]
+        sum = 0
+        for i in genes:
+            sum += i.allocatedBudgetValue
+        for i in genes:
+            if sum > totalBudget or i.allocatedBudgetValue < float(i.lowerBound) or\
+                    i.allocatedBudgetValue > (float(i.upperBound)*totalBudget)/100:
+                return False
 
     def fixChromosome(self, chromosome):
         pass
-
 
 class Gene:
     ROI = 0
@@ -117,18 +176,17 @@ class Gene:
     upperBound = ""
     lowerBound = ""
 
-
 class Chromosome:
     Genes = []
     totalBudget = 0
     fitness = 0
     numberOfChannels = 0
 
-    def __init__(self, genes, numberOfChannel, totalBudgets):
-        self.numberOfChannels = numberOfChannel
-        self.totalBudget = totalBudgets
-        self.Genes = genes
-        self.Fitness()
+    def __init__(self, Genes, numberOfChannels,totalBudget):
+        self.numberOfChannels = numberOfChannels
+        self.totalBudget = totalBudget
+        self.Genes = Genes
+        self.fitness = self.Fitness()
 
     def SetGenes(self, genes):
         self.Genes = genes
@@ -144,7 +202,6 @@ class Chromosome:
         for gene in self.Genes:
             d.append(gene.allocatedBudgetValue)
         print(d, " Fitness : ", self.fitness)
-
 
 if __name__ == '__main__':
     print("Enter the marketing budget (in thousands): ")
@@ -188,7 +245,6 @@ if __name__ == '__main__':
     for chromosome in chromosmeList1:
         chromosome.showChromosome()
 
-
     # n = 0
     # while n < 5000:
     #     ga = GeneticA(0.5 , 0.3)
@@ -201,3 +257,26 @@ if __name__ == '__main__':
 
 # Constraints
 # 1: sum of genes <= total budget
+
+    # gene1 = Gene()
+    # gene1.allocatedBudgetValue = 36
+    # gene1.lowerBound = 2.7
+    # gene1.upperBound = 58
+    #
+    # gene2 = Gene()
+    # gene2.allocatedBudgetValue = 50
+    # gene2.lowerBound = 20.5
+    # gene2.upperBound = 60
+    # genesList = [gene1,gene2]
+    #
+    # chromo = Chromosome(genesList,4,100)
+    # ga = GeneticA(0.5,0.1)
+    # ga.checkConstraints(chromo,100)
+    # ga.nonUniformMutation(chromo,1,5,1,0.5)
+
+
+
+
+
+
+
